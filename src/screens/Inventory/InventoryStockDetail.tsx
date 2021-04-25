4db1cc53136b0f5e3b633d8b225a1b7373f8982d
Stefan Amarie <stefan.amarie@zopa.com>
2021-04-25 18:56:00 +0200

    create InventoryItem

---
+++ src/screens/Inventory/InventoryStockDetail.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useInventoryContext } from "./InventoryContext";
+import InventoryItem from "./InventoryItem";
+
+const styles = StyleSheet.create({
+  scanner: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+});
+const StockDetail: FC = () => {
+  const { top } = useSafeAreaInsets();
+  const { stock } = useInventoryContext();
+
+  return (
+    <View style={[styles.container, { marginTop: top }]}>
+      {stock?.map((stockItem, key) => {
+        return <InventoryItem stockItem={stockItem} />;
+      })}
+    </View>
+  );
+};
+
+export default StockDetail;
